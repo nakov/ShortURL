dd6024706f797f63c9d724f9ce87cca813312553
Svetlin Nakov <svetlin@nakov.com>
2021-02-18 12:23:14 +0000

    Improved startup & port configuration

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ mvcController.setup(app, data);
 const apiController = require("./controllers/api-controller");
 apiController.setup(app, data);
 
-app.listen(8080)
+let port = process.argv[2];
+if (!port) port = process.env['PORT'];
+if (!port) port = 8080;
+
+app.listen(port, () => {
+  console.log(`App started. Listening at http://localhost:${port}`);
+})
 .on('error', function(err) {
   if (err.errno === 'EADDRINUSE')
-     console.error("Port 8080 busy (server already started).");
+    console.error(`Port ${port} busy.`);
   else 
     throw err;
 });
