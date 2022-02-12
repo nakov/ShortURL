509e50b1a80f60070a03731164506ba9ae4cd16a
Svetlin Nakov <nakov@users.noreply.github.com>
2022-02-12 15:52:43 +0000

    Improved the UI

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'))
+app.set('view engine', 'pug');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.static('public'))
-app.set('view engine', 'pug');
 
 const data = require("./data/app-data");
 data.seedSampleData();
